45055ce627a8b4efe5a0aa7302708d9275306755
rkddbs1031 <rkddbs1031@gmail.com>
2022-06-21 17:18:28 +0900

    ADD:  Main Component

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -1,14 +1,46 @@
+import { NavLink } from 'react-router-dom'
+
+import ProfileLeftImg from 'assets/img/profile_left.png'
+import ProfileRightImg from 'assets/img/profile_right.png'
+import { ProjectTextSvg, AboutTextSvg } from 'assets/svgs'
 import Contents from 'routes/_components/Contents'
 import Footer from './Footer'
 import styles from './main.module.scss'
+
+const MAIN_ITEM = [
+  {
+    id: 1,
+    item: 'left',
+    href: 'project',
+    img: ProfileLeftImg,
+    svg: <ProjectTextSvg />,
+  },
+  {
+    id: 2,
+    item: 'right',
+    href: 'about',
+    img: ProfileRightImg,
+    svg: <AboutTextSvg />,
+  },
+]
 
 const Main = () => {
   return (
     <>
       <Contents>
         <section className={styles.mainContainer}>
-          <div className={styles.left}>left</div>
-          <div className={styles.right}>right</div>
+          <ul>
+            {MAIN_ITEM.map((item) => (
+              <li key={item.id} className={styles[item.item]}>
+                <NavLink to={item.href}>
+                  <div className={styles.imgWrap}>
+                    <img src={item.img} alt={item.item} />
+                  </div>
+                  <div className={styles.svgWrap}>{item.svg}</div>
+                </NavLink>
+              </li>
+            ))}
+          </ul>
         </section>
       </Contents>
       <Footer />
