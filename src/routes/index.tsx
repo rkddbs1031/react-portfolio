377d62b40fc2cafe5e4aeea972f1493c961d0a2a
rkddbs1031 <rkddbs1031@gmail.com>
2022-06-22 03:29:52 +0900

    ING: About component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './_components/Header'
 import Main from './Main'
 import Projects from './Projects'
+import About from './About'
 import './routes.module.scss'
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='projects' element={<Projects />} />
+        <Route path='about' element={<About />} />
       </Routes>
     </>
   )
