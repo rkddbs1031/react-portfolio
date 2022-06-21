1f9863c40494a2f9115a2feb24cd012ad24407a1
rkddbs1031 <rkddbs1031@gmail.com>
2022-06-22 03:26:39 +0900

    ADD: util footer

---
+++ src/utils/footer.ts
@@ -0,0 +1,57 @@
+const FOOTER_TOP = [
+  {
+    id: 1,
+    key: 'projects',
+    items: [
+      {
+        id: 11,
+        text: 'Home',
+        href: '/',
+      },
+      {
+        id: 12,
+        text: 'About',
+        href: '/about',
+      },
+    ],
+  },
+  {
+    id: 2,
+    key: 'about',
+    items: [
+      {
+        id: 21,
+        text: 'Project',
+        href: '/projects',
+      },
+      {
+        id: 22,
+        text: 'Contact',
+        href: '/contact',
+      },
+    ],
+  },
+]
+
+const FOOTER_INFO = [
+  {
+    id: 1,
+    text: 'Front-end Developer : LEE KANG YOON',
+  },
+  {
+    id: 2,
+    text: 'PHONE : 010.9198.4389',
+  },
+  {
+    id: 3,
+    text: 'E-MAIL : rkddbs1031@gmail.com',
+  },
+]
+
+const getFooterTop = (path: string) => {
+  const NEW_LIST = FOOTER_TOP.filter((item) => {
+    return item.key === path
+  })
+  return NEW_LIST[0].items
+}
+export { FOOTER_TOP, FOOTER_INFO, getFooterTop }
