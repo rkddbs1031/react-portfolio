f74d32716f8ffc909eb5d71e6e57e17a53c87e7a
rkddbs1031 <rkddbs1031@gmail.com>
2022-06-23 00:55:43 +0900

    MOD: project type

--- src/types/projects.d.ts
+++ src/types/projects.d.ts
@@ -8,3 +8,37 @@ export interface IFotoerTop {
   text: string
   href: string
 }
+
+export interface ISpan {
+  span: string
+}
+
+export interface ITitle {
+  id: number
+  text: string
+}
+
+export interface IDescription {
+  text: string
+}
+
+export interface IUrl {
+  id: number
+  href: string
+  btn: string
+}
+
+export interface IProject {
+  id: number
+  num: string
+  desc: string
+  h2: {
+    text1: string
+    text2: string
+  }
+  urls: IUrl[]
+  img: {
+    src: string
+    alt: string
+  }
+}
