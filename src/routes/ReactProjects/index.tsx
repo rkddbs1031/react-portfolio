4a588c61efee68b89bce9f6684cf4d52d7e63df3
rkddbs1031 <rkddbs1031@gmail.com>
2022-06-23 00:58:36 +0900

    MOD: react project component   ProjectContnets 적용

--- src/routes/ReactProjects/index.tsx
+++ src/routes/ReactProjects/index.tsx
@@ -1,25 +1,29 @@
 import { useState, useMount } from 'hooks'
 
-import { ISpan, ITitle, IDescription } from 'types/projects'
-import { getAniInfo, getTitleInfo, getDescriptionInfo } from 'utils/projects'
+import { ISpan, ITitle, IDescription, IProject } from 'types/projects'
+import { getAniInfo, getTitleInfo, getDescriptionInfo, getProjectInfo } from 'utils/projects'
 
 import ProjectTitle from 'routes/_components/Contents/ProjectTitle'
+import ProjectContents from 'routes/_components/Contents/ProjectContents'
 import Footer from 'routes/_components/Footer'
 
 const ReactProjects = () => {
   const [aniInfo, setAniInfo] = useState<ISpan[]>([])
   const [titleInfo, setTitleInfo] = useState<ITitle[]>([])
   const [description, setDescription] = useState<IDescription>()
+  const [projectInfo, setProjectInfo] = useState<IProject[]>([])
 
   useMount(() => {
     setAniInfo(getAniInfo('react'))
     setTitleInfo(getTitleInfo('react'))
     setDescription(getDescriptionInfo('react'))
+    setProjectInfo(getProjectInfo('react'))
   })
 
   return (
     <>
       <ProjectTitle ANI_INFO={aniInfo} TITLE_INFO={titleInfo} DESCRIPTION={description} />
+      <ProjectContents PROJECT_INFO={projectInfo} />
       <Footer path='react' />
     </>
   )
